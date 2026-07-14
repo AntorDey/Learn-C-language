a22a63ccf3fedac442dbdc2f1b56136b09a68357
My DSA journey <antordey2005@gmail.com>
2026-07-14 22:06:59 +0600

    update pi

--- Syntex.c/volume_of_sphere.c
+++ Syntex.c/volume_of_sphere.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
         int r=5;
-        float v = (4*3.1416*r*r*r)/3;
+        float pi = 3.1416;
+        float v = (4*pi*r*r*r)/3;
         printf("The volume is: %f",v);
 
         return 0;
