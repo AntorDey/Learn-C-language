8e3a9a1a6d51a414abeb272e0856de4ccd0320e8
My DSA journey <antordey2005@gmail.com>
2026-07-14 20:46:25 +0600

    float data type calculation

---
+++ Syntex.c/calculation.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    float x= 5.00;
+    
+    float y=3.00;
+    printf("%.2f \n", x + y);
+    printf("%.2f \n", x - y);
+    printf("%.2f \n", x * y);
+    printf("%.2f \n", x / y);
+
+    return 0;
+}
