cf3c0df60e52c5deb5de4ff6299990ba27267602
My DSA journey <antordey2005@gmail.com>
2026-07-15 02:20:54 +0600

    remainder using modulous

---
+++ Syntex.c/remainder.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a,b;  //a>b
+    printf("Enter Dividend: ");
+    scanf("%d",&a);
+
+    printf("Enter Divisor: ");
+    scanf("%d",&b);
+
+    //int q = a/b;
+    int r = a%b;  //diviser*quotuent+remainder= divident.
+    printf("The remainder is : %d",r);
+
+        return 0;
+
+    }
