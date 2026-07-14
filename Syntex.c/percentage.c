44354ae87df219e5f11f47867add9ff57dd11973
My DSA journey <antordey2005@gmail.com>
2026-07-14 21:40:14 +0600

    percentage wala math

---
+++ Syntex.c/percentage.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float m1 = 100; // maths marks
+    float m2 = 77;  // physics marks
+    float m3 = 92;  // chem marks
+    float m4 = 90;  // english marks
+    float m5 = 86;  // bio marks
+    float p =(m1+m2+m3+m4+m5)/5;
+    printf("%f",p);
+
+}
