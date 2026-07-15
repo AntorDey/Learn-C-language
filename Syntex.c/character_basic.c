c7007e2c757c6b4130516e4101bf85d7fac87615
My DSA journey <antordey2005@gmail.com>
2026-07-15 22:07:00 +0600

    charecter

---
+++ Syntex.c/character_basic.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    char ch = 'A';
+    printf ("%c",ch);
+      // %d for ASCII vallue; 
+    // and if you tyoe %c then will print charecter
+    
+    return 0;
+}
